0f1a3b7e3a5a8171bd36167cd7b885fa3f91d8bf
ghedo44 <federicoghedini@gmail.com>
2026-06-16 16:04:24 +0200

    astro

--- src/types/blog.ts
+++
@@ -1,9 +0,0 @@
-export interface Post {
-    title: string;
-    description: string;
-    link: string;
-    slug: string;
-    githubLink?: string;
-    previewVideo: string;
-    technologies: string[];
-  }
